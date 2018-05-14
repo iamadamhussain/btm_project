aa91ab8eee847db72b26ad86feb0f69f5e469a78
Adam Hussain <iamadamhussain@gmail.com>
2018-05-14 09:43:08 +0530

    Add files via upload

---
+++ emp.sql
@@ -0,0 +1,16 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.0.19-nt 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `employee` (
+	`ID` int ,
+	`FIRSTNAME` varchar ,
+	`LASTNAME` varchar ,
+	`TELEPHONE` varchar ,
+	`EMAIL` varchar ,
+	`CREATED` timestamp 
+); 
+insert into `employee` (`ID`, `FIRSTNAME`, `LASTNAME`, `TELEPHONE`, `EMAIL`, `CREATED`) values('2','hussain','jjj','777','jjj','2018-05-14 09:39:11');
